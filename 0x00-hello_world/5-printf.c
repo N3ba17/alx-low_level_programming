7ca97585539462e4496e3238b9b0717f651354b1
N3ba17 <nebyu.tito17@gmail.com>
2023-09-09 03:37:56 -0700

    adding print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/*
+ *main -this is the starting point of main
+ *
+ * this fucntion print a messege
+ *
+ * Return: main is int of zero
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return(0);
+}

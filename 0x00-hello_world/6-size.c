a60ed91235e710a7d2fe55d8e993765e8651b348
N3ba17 <nebyu.tito17@gmail.com>
2023-09-09 04:17:24 -0700

    new sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - this is the main fuction of this code
+ *
+ * Description - for main function starts here
+ *
+ * Return: main returns int
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu bytes(s)\n", sizeof(char));
+	printf("Size of an int: %lu bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long));
+	printf("Size of a float: %lu bytes(s)\n", sizeof(float));
+	return (0);
+}

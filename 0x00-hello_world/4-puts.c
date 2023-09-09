268aadd30945e1cd539b32a0876ec6e6796a3b88
N3ba17 <nebyu.tito17@gmail.com>
2023-09-09 03:25:00 -0700

    correction please

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
-int main(void)
+
+/**
+ * main - this is the main fuction of this code
+ *
+ * Description - for main function starts here
+ *
+ * Return - just return nothing
+ */
+
+int main(void) 
 {
 	const char message[] = "\"Programming is like building a multilingual puzzle";
 		puts(message);

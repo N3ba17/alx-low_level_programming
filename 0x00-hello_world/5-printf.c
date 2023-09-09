efcfeb49fc8453bd22f7c32532ed774f167d8c37
N3ba17 <nebyu.tito17@gmail.com>
2023-09-09 03:53:51 -0700

    correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
-/*
- *main -this is the starting point of main
+
+/**
+ * main - this is the main fuction of this code
  *
- * this fucntion print a messege
+ * Description - for main function starts here
  *
- * Return: main is int of zero
-*/
+ * Return: main returns int
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
-	return(0);
+	return (0);
 }

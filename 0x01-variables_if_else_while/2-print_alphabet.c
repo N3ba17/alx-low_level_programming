2802d09ab523244d286359a6476a026fd746a419
N3ba17 <nebyu.tito17@gmail.com>
2023-09-11 00:28:01 -0700

    print character

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - this is main fuction
+ *
+ * Description - main function for displaying all small letters and start here
+ *
+ * Return: return int  for main function
+*/
+
+int main(void)
+{
+	int character;
+
+	for (character = 97; character <= 122; character++)
+	{
+		putchar(character);
+		putchar('\n');
+	}
+	return (0);
+}

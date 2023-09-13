77c44086789f16fbd10464693b79b08bdbb7da24
N3ba17 <nebyu.tito17@gmail.com>
2023-09-13 14:08:17 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * times_table - absolute value
+ *
+ * Description - 3-islower 10 check if letter is lower case
+ *
+ * Return: doesn't return
+*/
+
+void times_table(void)	
+{
+int i;
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			_putchar((j*i)+48);
+		}
+		_putchar('\n\);
+	}
+}

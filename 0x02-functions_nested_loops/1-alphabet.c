278f27832e72c60715ab23ebdd4a1d2b42f17435
N3ba17 <nebyu.tito17@gmail.com>
2023-09-13 10:29:55 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_alphabet - just print all character of small letters
+ *
+ * Description - print_alphabet for print alphabet
+ *
+ * Return: return int  for main function
+*/
+
+void print_alphabet(void)
+{
+int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
+/**
+ * main -this is entry point for main function
+ *
+ * Description - main function call print_alphabet function
+ *
+ * Return: return int to system
+*/
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}

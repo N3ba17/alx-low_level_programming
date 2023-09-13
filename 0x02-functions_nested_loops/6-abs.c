8649365bc2503315e40cae6058d6aa17ca66005f
N3ba17 <nebyu.tito17@gmail.com>
2023-09-13 12:56:55 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _abs - absolute value
+ *
+ * Description - 3-islower 10 check if letter is lower case
+ *
+ *@c: is int
+ *
+ * Return: return int  for main function
+*/
+
+int _abs (int c)
+{
+int temp;
+	if (c > 1)
+	{
+		return (c);
+	}
+	if (c == 0)
+	{
+		return (0);
+	}
+	if (c < 1)
+	{
+		temp = c * -1;
+		return (temp);
+	}
+return (0);
+}

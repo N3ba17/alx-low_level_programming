94c9e58c7e892b57416d9bb9ce526b33932ee977
N3ba17 <nebyu.tito17@gmail.com>
2023-09-13 12:10:24 -0700

    is it alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _isalpha - check if character is lower case
+ *
+ * Description - 3-islower 10 check if letter is lower case
+ *
+ *@c: is character rep
+ *
+ * Return: return int  for main function
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	if (c >=65 && c <=90)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}

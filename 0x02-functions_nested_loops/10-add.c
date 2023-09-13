6bb732b2860a6a8becdc31aecdd0d1b6700bc595
N3ba17 <nebyu.tito17@gmail.com>
2023-09-13 14:54:33 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - absolute value
+ *
+ *@x: first description
+ *
+ *@y: second description
+ *
+ * Description - 3-islower 10 check if letter is lower case
+ *
+ * Return: int 
+*/
+int add(int x, int y)
+{
+return (a + b);
+}
+

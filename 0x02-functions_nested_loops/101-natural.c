cb4ae14fe383f167e5edffff14121d989fe0bd8d
N3ba17 <nebyu.tito17@gmail.com>
2023-09-14 16:28:52 -0700

    1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print sum of number that are divisible by 3 and 5
+ *
+ * Description - print_alphabet for print alphabet
+ *
+ * Return: return int
+*/
+
+int main (void)
+{
+int j, result;
+
+	for (j = 0; j <= 1024; j++)
+	{
+		if (j % 3 == 0 || j % 5 == 0)
+		{
+			result = result + j;
+		} 
+	}
+printf("%d\n",result);
+return (0);
+}
+

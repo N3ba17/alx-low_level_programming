b5e745126ab17a53538b70abafcd1a86b7e4d3d8
N3ba17 <nebyu.tito17@gmail.com>
2023-09-16 11:15:19 -0700

    correction

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,13 @@
-#inlude "main.h"
+#include "main.h"
 /**
- * _isupper check if upper case
- *
+ * _isupper - check if upper case
+ * @c : integer
  * Return: int 1 if true and  0 if false
- *
+ * Description - check if c is upper case
  */
 int _isupper(int c)
 {
-	if (c > 65 || c <90)
+	if (c > 65 || c < 0)
 	{
 		return (1);
 	{

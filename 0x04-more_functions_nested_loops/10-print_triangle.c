a4dc08baee351324a5810079c8ebd38841ec110c
N3ba17 <nebyu.tito17@gmail.com>
2023-09-16 16:36:58 -0700

    final

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_triangle - print triangle
+ * Return: no return
+ * @size: int from main
+ * Description - print triangle
+ */
+void print_triangle(int size)
+{
+int i, j, m, n;
+
+n = 1;
+m = size - 1;
+if (size > 0)
+{
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			_putchar(' ');
+		}
+		for (j = 0; j < n; j++)
+		{
+			_putchar('#');
+		}
+		if (i != size -1)
+		{
+			_putchar('\n');
+		}
+		m--;
+		n++;
+	}
+}
+_putchar('\n');
+}

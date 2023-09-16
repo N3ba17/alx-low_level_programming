ae133b6165bf150ade4030cb5e5a24a3c10aedae
N3ba17 <nebyu.tito17@gmail.com>
2023-09-16 15:08:26 -0700

    correction

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,14 +7,18 @@
  */
 void print_diagonal(int n)
 {
-int i;
+int i, m;
 
 if (n > 0)
 {
 	for (i = 0; i < n; i++)
 	{
+		for (m = 0; m < i; m++)
+		{
+			_putchar(' ');
+		}
 		_putchar(92);
+		_putchar('\n');
 	}
 }
-_putchar('\n');
 }

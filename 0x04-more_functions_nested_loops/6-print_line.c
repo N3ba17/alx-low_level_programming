1eedd082767d753a5f4be0b162c2448cca076038
N3ba17 <nebyu.tito17@gmail.com>
2023-09-16 14:40:25 -0700

    print a line)

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - check if upper case
+ * Return: no return
+ * @n: int from main
+ * Description - print line
+ */
+void print_line(int n)
+{
+int i;
+
+if (n > 0)
+{
+	for (i = 0; i <= n; i++)
+	{
+		_putchar(95);
+	}
+}
+_putchar('\n');
+}

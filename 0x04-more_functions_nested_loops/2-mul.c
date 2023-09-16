27dff7cec59f9aca93a5c328a7637a0ec5483f02
N3ba17 <nebyu.tito17@gmail.com>
2023-09-16 12:17:47 -0700

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - check if upper case
+ * @c : integer
+ * Return: int 1 if true and  0 if false
+ * Description - check if c is upper case
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

f60e73837964be90dca8c3d97582c23fbbfe8415
N3ba17 <nebyu.tito17@gmail.com>
2023-09-16 11:08:34 -0700

    is it upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#inlude "main.h"
+/**
+ * _isupper check if upper case
+ *
+ * Return: int 1 if true and  0 if false
+ *
+ */
+int _isupper(int c)
+{
+	if (c > 65 || c <90)
+	{
+		return (1);
+	{
+	else
+		return (0);
+}

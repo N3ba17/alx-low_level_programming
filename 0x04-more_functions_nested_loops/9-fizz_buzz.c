18a3f5faf38e6dc2fc46f68d7ab6fd5bba6fb9d7
N3ba17 <nebyu.tito17@gmail.com>
2023-09-16 15:56:19 -0700

    fizz buzz correction

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+/**
+ * main - fizz buzz
+ * Description - fizz buzz
+ * Return: integer
+ */
+
+int main(void)
+{
+int i;
+	for (i = 1; i <= 100; i++)
+	{
+		if (i != 100 || i != 1)
+		{
+			printf(" ");
+		}
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+			continue;
+		}
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+			continue;
+		}
+		if (i % 5 == 0)
+		{
+			printf("Buzz");
+			continue;
+		}
+		printf("%d", i);
+	}
+printf("\n");
+}

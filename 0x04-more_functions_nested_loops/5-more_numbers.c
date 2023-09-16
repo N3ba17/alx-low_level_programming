303cd949b13a76ba49f97199f42582806f898f1c
N3ba17 <nebyu.tito17@gmail.com>
2023-09-16 13:23:18 -0700

    more

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * more_numbers - check if upper case
+ * Return: no return
+ * Description - print number
+ */
+void more_numbers(void)
+{
+int i, j, k;
+
+k = 10;
+for (j = 0; j < 10; j++)
+{
+	for (i = 0; i < 14; i++)
+	{
+		if (i <= 9)
+		{
+			_putchar(i);
+		}
+		else
+		{
+			_putchar(k);
+			k++;
+		}
+	}
+	k = 10;
+}
+_putchar('\n');
+}

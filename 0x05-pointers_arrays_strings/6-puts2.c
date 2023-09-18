65a39f3cc08b365559a34249bddb9c9bb7aa9842
N3ba17 <nebyu.tito17@gmail.com>
2023-09-18 14:18:57 -0700

    skip 1 value

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_strlen - length of string
+ *@s: string var
+ *Return: none
+ */
+int _puts2(char *str)
+{
+	int i;
+
+	int len = 0;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	for (i = 0;i < len; i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}

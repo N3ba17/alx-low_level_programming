d067134f14021fb2d4629aaae334e557388579cd
N3ba17 <nebyu.tito17@gmail.com>
2023-09-18 11:14:52 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * reset_to_98 - takes pointer change value to 98
+ *
+ * Return: none
+ *
+ * @n: pointer to n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+directory for pointer and array

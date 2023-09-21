24afd8528b4a007d54bc13a19fbbb4204e9cb102
N3ba17 <nebyu.tito17@gmail.com>
2023-09-21 13:27:04 -0700

    string compair

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include"main.h"
+/**
+ * _strcmp - combine two strings
+ *@s1: first parameter
+ *@s2: second parameter
+ *Return: int
+*/
+int _strcmp(char *s1, char *s2)
+{
+int l, result;
+l = 0;
+result = 0;
+while (s1[l] != '\0' && s2[l] != '\0')
+{
+	if(s1[l] != s2[l])
+	{
+		result = s1[l] - s2[l];
+		break;
+	}
+	l++;
+}
+return (result);
+}

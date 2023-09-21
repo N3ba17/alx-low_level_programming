996f7d92c8ed9401dc180df33c477b64d06b475a
N3ba17 <nebyu.tito17@gmail.com>
2023-09-21 15:15:28 -0700

    cap with exception

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ *cap_string - combine two strings
+ *@v: first parameter
+ *Return: pointer to char
+*/
+char *cap_string(char *v)
+{
+int l = 0;
+while (v[l] != '\n')
+{
+	if (v[l] >= 97 && v[l] <= 122)
+	{
+		if (l == 0)
+		{
+			v[l] -= 32;
+		}
+		if (v[l - 1] == 9 || v[l - 1] == 10 || v[l - 1] == 32 || v[l - 1] == 33 || v[l - 1] == 34 || v[l - 1] == 40 ||v[l - 1] == 41 || v[l - 1] == 44 || v[l - 1] == 46 || v[l - 1] == 59 || v[l - 1] == 63 || v[l - 1] == 123 || v[l - 1] == 124)
+{
+			v[l] -= 32;
+}
+	l++;
+}
+}
+return (v);
+}

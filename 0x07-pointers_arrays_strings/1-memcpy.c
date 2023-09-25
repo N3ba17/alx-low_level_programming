51fad48be82bdb18d1cea2873cf191043b2bb406
Nebyu Tito <nebyu.tito17@gmail.com>
2023-09-25 15:09:25 -0400

    rename

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_memcpy - copy memory area
+ * Return: pointer to memory address
+ *@dest: first item
+ *@src: second item
+ *@n: third itme
+ */
+
+char *_memcpy (char *dest, *src, unsigned int n)
+{
+
+}
+

d4c08d65e7dffaa40a47aa1666a9df0e2ed0c347
Nebyu Tito <nebyu.tito17@gmail.com>
2023-09-25 16:29:45 -0400

    hexa

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_memset - fill memory with constant byte
+ *@s: first item
+ *@b: second item
+ *@n: third item
+ *Return: pointer to memory
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}

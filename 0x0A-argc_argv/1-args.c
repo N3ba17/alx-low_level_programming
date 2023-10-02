2c039e7a7cb6f2054c97a906b098b26c16c7b988
Nebyu Tito <nebyu.tito17@gmail.com>
2023-10-02 16:00:34 -0400

    count how many var

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ *main - return argc
+ *@argc: int
+ *@argv:char
+ *Return: int
+*/
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}

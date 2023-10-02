f6bb53eab46f79dd134d1518b6300d1e4e6ddf43
Nebyu Tito <nebyu.tito17@gmail.com>
2023-10-02 18:13:38 -0400

    corrections

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,8 +2,13 @@
 #include<stdlib.h>
 int main(int argc, char *argv[])
 {
+if (argc != 2)
+{
+	printf("Error\n");
+	return (1);
+}
 int i, cents, x;
-int test[5]={25,10,5,2,1};
+int test[5] = {25, 10, 5, 2, 1};
 
 x = atoi(argv[1]);
 for (i = 0; i < 5; i++)
@@ -14,8 +19,7 @@ for (i = 0; i < 5; i++)
 		cents += 1;
 	}
 }
-printf("%d\n",cents);
-
+printf("%d\n", cents);
 
 return (0);
 }

3eaf1a2edf70911a507f0fae5c8d0b57d21d9660
Nebyu Tito <nebyu.tito17@gmail.com>
2023-10-02 18:20:21 -0400

    new corrections

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
+/**
+ *main - cents
+ *@argc: number
+ *@argv: string
+ *Return: none
+*/
 int main(int argc, char *argv[])
 {
 int i, cents, x;
-int test[5]={25,10,5,2,1};
+int test[5] = {25, 10, 5, 2, 1};
 if (argc != 2)
 {
 	printf("Error\n");

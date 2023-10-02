cc3f233ac33d0fd2f4078332871110707970ab22
Nebyu Tito <nebyu.tito17@gmail.com>
2023-10-02 16:11:01 -0400

    print all var

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ *main - return argc
+ *@argc: int
+ *@argv:char
+ *Return: int
+*/
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	int x = 1;
+
+	while (x < argc)
+	{
+		printf("%s \n", argv[x]);
+		x++;
+	}
+	return (0);
+}

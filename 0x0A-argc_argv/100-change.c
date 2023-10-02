9790600990207ab6479dc7418afd15336bc0a617
Nebyu Tito <nebyu.tito17@gmail.com>
2023-10-02 18:17:11 -0400

    final

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,14 +2,13 @@
 #include<stdlib.h>
 int main(int argc, char *argv[])
 {
+int i, cents, x;
+int test[5]={25,10,5,2,1};
 if (argc != 2)
 {
 	printf("Error\n");
 	return (1);
 }
-int i, cents, x;
-int test[5] = {25, 10, 5, 2, 1};
-
 x = atoi(argv[1]);
 for (i = 0; i < 5; i++)
 {

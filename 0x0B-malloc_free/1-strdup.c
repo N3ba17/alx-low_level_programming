a08da38950c63cf4a7d07893fb3629d6362f9c32
Nebyu Tito <nebyu.tito17@gmail.com>
2023-10-03 16:36:42 -0400

    copy string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include"main.h"
+/**
+ *_strdup - returns pointer to newly allocated space in memory
+ *@str: string from main
+ *Return: pointer
+ */
+char *_strdup(char *str)
+{
+char *assign;
+unsigned int i, x;
+
+if (str == NULL)
+{
+	return (NULL);
+}
+while(str[x] != '\0')
+{
+	x++;
+}
+assign = (char *) malloc(sizeof(char) * (i + 1));
+if (assign == NULL)
+{
+	return (NULL);
+}
+for (i = 0;i <= x;i++)
+{
+	assign[i] = srt[i];
+}
+return (assign);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,3 +1,4 @@
 #include<stdio.h>
 #include<stdlib.h>
 char *create_array(unsigned intsize, char c);
+char *_strdup(char *str);

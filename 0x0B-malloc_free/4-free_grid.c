eb0715bbdf97e6b8c7dd1cc24fdecbd641d72f56
Nebyu Tito <nebyu.tito17@gmail.com>
2023-10-04 23:43:54 +0300

    removing white space

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,20 +1,20 @@
 #include"main.h"
 /**
- * free_grid - free the grid setup previous 
+ * free_grid - free the grid setup previous
  *@grid: first argument
  *@height: second argument
  */
 void free_grid(int **grid, int height)
 {
 	int x;
+
 	if (grid == NULL)
 	{
 		return;
 	}
-	for (x = 0;x < height; x++)
+	for (x = 0; x < height; x++)
 	{
 		free(grid[x]);
 	}
 	free(grid);
 }
- 

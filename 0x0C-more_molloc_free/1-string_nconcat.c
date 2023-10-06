b690617a631627712ae15563880a27736b3ebb83
Nebyu Tito <nebyu.tito17@gmail.com>
2023-10-06 16:40:22 -0400

    combine with parameter

--- 0x0C-more_molloc_free/1-string_nconcat.c
+++ 0x0C-more_molloc_free/1-string_nconcat.c
@@ -8,5 +8,30 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	return (s1);
+	int i = 0, j = 0, x;
+	char *new_p;
+
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+	if (j >= n)
+	{
+		j = n;
+	}
+	new_p = malloc(sizeof(char) * (i + j + 1));
+	for (x = 0; x < i; x++)
+	{
+		new_p[x] = s1[x];
+	}
+	for (x = 0; x < j; x++)
+	{
+		new_p[i + x] = s2[x];
+	}
+	new_p[i + j] = '\0';
+	return (new_p);
 }

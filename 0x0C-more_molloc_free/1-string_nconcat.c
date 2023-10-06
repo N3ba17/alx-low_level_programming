e013aa39e819e1b61a80bdee2a9f6ba22f9ac989
Nebyu Tito <nebyu.tito17@gmail.com>
2023-10-06 15:12:21 -0400

    checking for file existance

---
+++ 0x0C-more_molloc_free/1-string_nconcat.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ *string_nconcat - concatenates two string
+ *@s1: first string
+ *@s2: second string
+ *@n: int
+ *Return: char pointer
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	return (s1);
+}

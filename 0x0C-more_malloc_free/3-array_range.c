72c325acaede2b89f92b9443f433166b21e2b0f9
Nebyu Tito <nebyu.tito17@gmail.com>
2023-10-07 16:57:48 -0400

    new aarray

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include"main.h"
+/**
+ * array_range - array of int
+ * @min: min
+ * @max: max
+ * Return: new array
+*/
+char *array_range(int min, int max)
+{
+	int i;
+	int *new;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	new = malloc(sizeof(int) * (max - min + 1));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; min <= max; i++)
+	{
+		new[i] = min;
+		min++;
+	}
+	return (new);
+}

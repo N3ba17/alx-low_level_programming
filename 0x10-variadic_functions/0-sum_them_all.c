e50c7f5dfba78b1ba885e4ac12e46ba0c5e45d86
Nebyu Tito <nebyu.tito17@gmail.com>
2023-10-12 21:42:49 -0400

    correction on style

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,10 +8,11 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list np;
 	unsigned int p, sum = 0;
+
 	va_start(np, n);
 	for (p = 0; p < n; p++)
 	{
-		sum = sum + va_arg(np,int);
+		sum = sum + va_arg(np, int);
 	}
 	va_end(np);
 	return (sum);

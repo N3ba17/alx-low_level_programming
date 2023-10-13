1f41eb544c19bcd0704a664a1c7bda444cac7c36
Nebyu Tito <nebyu.tito17@gmail.com>
2023-10-12 21:56:24 -0400

    correction on style for print

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,11 +10,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list np;
 	unsigned int i;
-	va_start(np,n);
+
+	va_start(np, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(np,int));
-		if (separator && i != n -1)
+		printf("%d", va_arg(np, int));
+		if (separator && i != n - 1)
 		{
 			printf("%s", separator);
 		}

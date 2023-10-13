bf6eefafdabfe465ea68647799c3c0b716b28ecb
Nebyu Tito <nebyu.tito17@gmail.com>
2023-10-12 21:40:54 -0400

    multiple args

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,18 @@
+#include<stdarg.h>
+/**
+* sum_them_all - sum all args
+*@n: number counter
+*Return: sum
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list np;
+	unsigned int p, sum = 0;
+	va_start(np, n);
+	for (p = 0; p < n; p++)
+	{
+		sum = sum + va_arg(np,int);
+	}
+	va_end(np);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,5 @@
+#ifndef variadic_functions_
+#define variadic_functions_
+
+int sum_them_all(const unsigned int n, ...);
+#endif

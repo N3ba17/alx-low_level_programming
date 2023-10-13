4ee608b6183c906ea79e03db889539d2617171e4
Nebyu Tito <nebyu.tito17@gmail.com>
2023-10-12 21:54:48 -0400

    print

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include<stdarg.h>
+#include<stdio.h>
+/**
+* print_numbers - sum all args
+*@n: number counter
+*@separator: -
+*Return: none
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list np;
+	unsigned int i;
+	va_start(np,n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(np,int));
+		if (separator && i != n -1)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(np);
+	printf("\n");
+}

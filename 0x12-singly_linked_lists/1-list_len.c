3d820fb084cd23f8d512b74445ab4bfc51919ccf
Nebyu Tito <nebyu.tito17@gmail.com>
2023-10-20 15:53:13 +0300

    length of string

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include"lists.h"
+
+/**
+ * list_len - return the number of element
+ * @h: first parameter
+ * Return: size_t
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+		{
+			h = h->next;
+			i++;
+		}
+	}
+	return (i);
+}
+
